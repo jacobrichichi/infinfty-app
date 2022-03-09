3b6da37c3c7ffdef37961cc42c0741f7aadac0c2
haksookim <gkrtn1024@gmail.com>
2022-03-09 14:15:32 -0500

    Add Login modal

--- react-ui/src/Components/Navbar.js
+++ react-ui/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import * as React from 'react';
 
 import logo from "./images/logo.png"
 import searchBarIcon from "./images/searchBarIcon.png"
@@ -10,7 +10,31 @@ import TextField from '@mui/material/TextField';
 import "./Navbar.css"
 
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const Navbar = (props) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const handleLogin = () => {setLoggedIn(true); setOpen(false);}
+
+
   return (
     <Grid container columns = {24} spacing = {1} id = "navBarContainer">
         <Grid item xs = {4} id = "logoContainer">
@@ -44,9 +68,52 @@ const Navbar = (props) => {
         </Grid>
         
         <Grid item xs = {3} id = "userTextContainer">
-            <div id = "userText">
-              Hi, Username
+          {open ?
+          <>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Grid container spacing={2}  direction="column" justifyContent="center" alignItems="center">
+                      <Grid item xs = {12}>
+                          <div >
+                            Login
+                          </div>
+                      </Grid>
+                      <Grid item xs = {12}>
+                          <input >
+                          </input>
+                      </Grid>
+                      <Grid item xs = {12}>
+                          <div >
+                            Password
+                          </div>
+                      </Grid>
+                      <Grid item xs = {12}>
+                          <input >
+                          </input>
+                      </Grid>
+                      <Grid item xs = {12}>
+                      <Button variant="outlined" onClick={handleLogin} >Login</Button>
+                      </Grid>
+                  </Grid>
+                </Box>
+              </Modal>
+          </>  
+            :  
+            loggedIn
+            ?
+            <div id = "userText" >
+              Hello User
             </div>
+            :
+            <div id = "userText" onClick={handleOpen}>
+              Login
+            </div>
+            }
         </Grid>
     </Grid>
   )
