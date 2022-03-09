48f8eaf5ab08a0230379b3c0277b37365662dcf5
Kevin Calle <kevin.calle@stonybrook.edu>
2022-03-09 16:11:50 -0500

    Sidebar for Account Settings

--- react-ui/src/Components/Account.js
+++ react-ui/src/Components/Account.js
@@ -2,9 +2,42 @@ import React from "react";
 import Navbar from "./Navbar";
 import Gridding from "./Gridding";
 import './General.css';
+import './Account.css';
+
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Grid } from "@mui/material";
+
 
 function Account() {
-    
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Grid container>
+
+                <Grid item xs = {4}>
+                    <ProSidebar>
+                        <Menu iconShape="square">
+                            <MenuItem>Account</MenuItem>
+                                <SubMenu title="Wallets">
+                                    <MenuItem>Wallet</MenuItem>
+                                    <MenuItem>Wallet</MenuItem>
+                                </SubMenu>
+                            <MenuItem>NFT</MenuItem>
+                        </Menu>
+                    </ProSidebar>  
+                </Grid>  
+
+                <Grid item xs>
+                    <div id="mainPage">
+                        <h1>ACCOUNT</h1>
+                    </div> 
+                </Grid> 
+
+            </Grid>
+
+        </div>
+    );
 }
 
 export default Account;
