c63fa4e445fa30e3e8d9b554551f543ab0f1c69e
jacobrichichi <jacobrichichi@yahoo.com>
2022-03-08 20:31:02 -0500

    Improved Landing

--- react-ui/src/Components/DesktopMainCard.js
+++ react-ui/src/Components/DesktopMainCard.js
@@ -1,10 +1,17 @@
 import React from "react"
 import squid from "./images/squidft.png"
+import axie from "./images/axie_pack.png"
+import sara from "./images/sarafranimage.jpg"
 import "./DesktopMainCard.css"
 import Grid from '@mui/material/Grid';
 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 const DesktopMainCard = (props) => {
+    
+
     return (
         <div style = {{paddingTop: "20px"}}>
             <div id = "background"/>
@@ -28,8 +35,51 @@ const DesktopMainCard = (props) => {
             </div>
 
             <div id = "previewCard">
-                <img src = {squid} id = "squid">
-                </img>
+                <Carousel showArrows={false} showIndicators = {false} showStatus = {false} showThumbs = {false} infiniteLoop = "true" autoPlay = {true} interval = {3000}>
+                    <div>
+                        <img src = {squid} id = "cardImage"/>
+                        <div id = "cardTextContainer">
+                            <div id = "cardTitleText">
+                                Squidwardo
+                            </div>
+                            <div id = "cardDescriptionText">
+                                This is a hot ticket item, tops of the tops!
+                            </div>
+                        </div>
+                    
+                    </div>
+
+                    <div>
+                        <img src = {sara} id = "cardImage"/>
+                        <div id = "cardTextContainer">
+                            <div id = "cardTitleText">
+                                3LADIES
+                            </div>
+                            <div id = "cardDescriptionText">
+                                Sara Franzese is an Italian post-modern artist with a keen eye for 
+                                capturing the fine details and contours of the human figure.
+                                Take a look at her latest piece here
+                            </div>
+                        </div>
+                    
+                    </div>
+
+                    <div>
+                        <img src = {axie} id = "cardImage"/>
+                        <div id = "cardTextContainer">
+                            <div id = "cardTitleText">
+                                Axie Infinity
+                            </div>
+                            <div id = "cardDescriptionText">
+                                Only the hottest NFT video game!
+                            </div>
+                        </div>
+                    
+                    </div>
+
+                    
+                </Carousel>
+
 
             </div>
         </div>
