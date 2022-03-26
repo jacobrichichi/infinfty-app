90f3abc7c69c4e90a1b332538266e220219539e8
jacobrichichi <jacobrichichi@yahoo.com>
2022-03-26 11:23:11 -0400

    Server

--- react-ui/src/auth/auth-request-api/index.js
+++ react-ui/src/auth/auth-request-api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'http://localhost:5000/auth',
+    baseURL: 'https://infinftapp.herokuapp.com/auth',
 })
 
 export const loginUser = (email, password) => {
